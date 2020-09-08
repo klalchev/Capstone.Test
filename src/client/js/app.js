c2e2cce6d1b13942c9bb0172059c66af7b2dbde8
Kostadin <kostadin.lalchev88@gmail.com>
2020-09-08 07:17:24 +0300

    Initial commit

---
+++ src/client/js/app.js
@@ -0,0 +1,127 @@
+function performAction(e){
+    // event.preventdefault()
+
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = (d.getMonth() + 1) + '.'+ d.getDate()+'.'+ d.getFullYear(); //0= january, so adding 1 to show the correct month
+    console.log(newDate);
+
+    // Declare apiKey
+    let userName = 'lalchev88';
+    const weatherBitKey = '7fa5a67defbd48f8a9001a8eff943b3a';
+
+    // Select the actual value of an HTML input to include in POST
+    const newCity = document.getElementById('city').value;
+    // const fav = document.getElementById('feelings').value;
+
+    let url = `http://api.geonames.org/searchJSON?q=${newCity}&maxRows=1&username=${userName}`; // could also be declared this way: `https://api.openweathermap.org/data/2.5/weather?zip=${newWeather},${countryCode}&appid=${apiKey}` countryCode will need to be declared as a variable in the performAction function
+
+
+    getWeatherDemo(url)
+    .then(function(data){   // the variable data declared in getWeatherDemo function. These are chained promises. function(data) passes the received data to the postData request
+
+    console.log(data);
+    postData('/addCity', {lat: data.geonames[0].lat, lng: data.geonames[0].lng, country: data.geonames[0].countryName, date: newDate, trip: newCity} ) //HOW TO ACCESS AN OBJECT WITHIN AN ARRAY WITHIN AN OBJECT: description: data.weather[2].description https://stackoverflow.com/questions/11922383/how-can-i-access-and-process-nested-objects-arrays-or-json
+    })
+        .then(res=>updateWeather(res))
+            .then(function(myData){
+            postData('/addWeatherBit', {temp: myData.data[0].temp, description: myData.data[0].weather.description})
+
+            updateUI()
+        })
+/*
+   const geonamesData =await getWeatherDemo(url);
+   const res = await postData('/addCity', {lat: geonamesData.geonames[0].lat, lng: geonamesData.geonames[0].lng, country: geonamesData.geonames[0].countryName, date: newDate, trip: newCity});
+   const myData = await updateWeather(res);
+   const weatherBitInfo = await postData('/addWeatherBit', {temp: myData.data[0].high_temp, description: myData.data[0].weather});
+   console.log(myData);
+   console.log(weatherBitInfo);
+
+   updateUI();
+*/
+}
+
+/* POST Example */
+const postData = async ( url = '', data = {})=>{
+    //console.log(data);
+    const response = await fetch(url, {
+    method: 'POST', //*GET, POST, PUT, DELETE - we could get data, post data, put or delete data
+    credentials: 'same-origin', //include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content- Type ". Here we attach our data to the body of our POST request
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+    console.log("error", error);
+    //appropriately handle the error
+    }
+}
+
+const getWeatherDemo = async (baseURL)=>{
+    //1.
+    const res = await fetch(baseURL)
+    //2. Call Fake API
+    //const res = await fetch('/fakePictureData')
+    try {
+
+        const data = await res.json(); // res.json() is the data you fetch
+        console.log(data)
+        return data;
+        // 1. We can do sth with our returned data here-- like chain promises
+
+        // 2.
+        // postData('/addAnimal', data)
+    }   catch(error) {
+        // appropriately handle the error
+        console.log("error", error);
+    }
+}
+
+
+const updateWeather = async (weatherData)=>{
+    //1.
+    const weatherBitKey = '7fa5a67defbd48f8a9001a8eff943b3a';
+    const res = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${weatherData.lat}&lon=${weatherData.lng}&key=${weatherBitKey}`)
+    //2. Call Fake API
+    //const res = await fetch('/fakePictureData')
+    try {
+
+        const data = await res.json(); // res.json() is the data you fetch
+        console.log(data)
+        return data;
+        // 1. We can do sth with our returned data here-- like chain promises
+
+        // 2.
+        // postData('/addAnimal', data)
+    }   catch(error) {
+        // appropriately handle the error
+        console.log("error", error);
+    }
+}
+
+
+/* Update UI Demo */
+const updateUI = async () => {
+    const request = await fetch('/all')
+    try{
+        const allData = await request.json();
+        console.log(allData);
+
+        document.getElementById('date').innerHTML ='Departing: ' + allData.date;
+        document.getElementById('temp').innerHTML ='Temperature: ' + allData.temp;
+        document.getElementById('trip').innerHTML ='My trip is to: ' + allData.trip + ', ' +allData.country;
+        document.getElementById('desc').innerHTML = 'Forecast: ' + allData.description;
+        document.getElementById('country').innerHTML ='Country: ' + allData.country;
+        //document.getElementById('content').innerHTML = 'Feeling: ' + allData.fav;
+    }catch(error){
+        console.log("error", error)
+    }
+}
+
+export {performAction}
---
+++ src/server/server.js
@@ -0,0 +1,82 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {}; // can not be cost because it is being reassigned each time we fetch a new zip code object
+
+
+var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const app = express()
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+// Initialize the main project folder
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+    // res.sendFile('client/views/index.html', {root: __dirname + '/..'})
+    // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(4040, function () {
+    console.log('Example app listening on port 4040!')
+})
+
+// Setup Server
+
+/*
+const port = 3000;
+
+const server = app.listen(port, listening); // listen method- when the server has been activated, log the `running on localhost: ${port}`message
+function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+    console.log('hello');
+};
+*/
+
+//GET Route
+
+app.get('/all', getData) //In this case get sends the data to the app.js. Every GET request produces a request, which is the data provided by the GET request, and a response, which is the data returned to the GET request
+
+function getData(req, res){
+    res.send(projectData)
+    console.log(projectData)
+}
+
+app.post('/addWeatherBit', getWeatherData)
+
+function getWeatherData(req, res){
+   projectData.temp= req.body.temp; // How to add the myNewEntry to the projectdata
+   projectData.description= req.body.description;
+
+   res.send(projectData) //send projectData to the server
+   console.log(projectData)
+}
+
+// POST Route
+
+app.post('/addCity', addCity); //post data from app.js to server.js
+
+function addCity(req, res){ //each route (post or get) callback function has a request (in this case req) and respond (res) parameters. Request requests/accesses data from the app. Respond sends data to the app
+
+    newEntry = {
+        lat: req.body.lat, //In the app file we attached our data to the body of our POST request, so to receive that data and make it actionable we can use req.body
+        lng: req.body.lng,
+        country: req.body.country,
+      // when you add a new element to the server, you have to rerun the server to show the new element
+      // fav: req.body.fav,
+        date: req.body.date,
+        trip: req.body.trip
+    }
+
+projectData = newEntry; // projectData can not be of type const, because we are updating it each time a new zip is fetched. projectDate will be reassigned the value of newEntry each time and hence it will only store the last entry, whereas an array with a push method would store alll entries. Since the object stores the last entry, it is light weight- does not occupy memory!
+res.send(projectData) //send projectData to the server
+console.log(projectData)
+}
+
